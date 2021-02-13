966089e32632307f23246a828eabb1481ccb38ee
Jiajun Bao <jiajunb@umich.edu>
2021-02-13 12:20:05 -0500

    added project structures

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code and models for the paper "Conversations Gone Alright: Quantifying and Predicting Prosocial Outcomes in Online Conversations" at the Web Conference 2021',
+    author='Jiajun Bao*, Junjie Wu*, Yiming Zhang*, Eshwar Chandrasekharan, and David Jurgens.',
+    license='',
+)
